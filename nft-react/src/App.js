92670cb35d48acb09f41d35e3ef5ef943c459df8
Saumik <saumikdhar@yahoo.co.uk>
2023-03-08 18:28:36 +0000

    Add nft fetch requests

--- nft-react/src/App.js
+++ nft-react/src/App.js
@@ -1,7 +1,11 @@
 import classes from './App.module.css';
+import { useEffect, useState } from 'react';
+import { YOUR_API_KEY } from './shared/utility';
 
 const App = () => {
-  const data = [
+  const [nftData, setNftData] = useState(null);
+
+  const sampleData = [
     ['NFT details', 'NFT 1'],
     ['NFT details', 'NFT 2'],
     ['NFT details', 'NFT 3'],
@@ -10,7 +14,7 @@ const App = () => {
     ['NFT details', 'NFT 6']
   ];
 
-  const arrayOfHelp = data.map((data, index) => (
+  const arrayOfNfts = sampleData.map((data, index) => (
     <div key={index} className={classes.box}>
       <div className={classes.image} />
       <div className={classes.contentWrapper}>
@@ -24,9 +28,33 @@ const App = () => {
     </div>
   ));
 
+  useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    fetch('https://api.opensea.io/v2/listings/collection/slug/all', {
+      method: 'POST',
+      header: 'X-API-KEY:' + { YOUR_API_KEY }
+    })
+      .then(async response => {
+        const data = await response.json();
+
+        setNftData(data);
+
+        if (!response.ok) {
+          const error = data.errors || response.statusText;
+          return Promise.reject(error);
+        }
+      })
+      .catch(error => {
+        console.log('There was an error! ', error);
+      });
+
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  }, []);
+
   return (
     <div className={classes.backgroundColour}>
-      <div className={classes.boxes}>{arrayOfHelp}</div>
+      <div className={classes.blockText}>Pick your NFT</div>
+      <div className={classes.boxes}>{arrayOfNfts}</div>
     </div>
   );
 };
